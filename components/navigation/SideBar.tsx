618eeb3b4b11cfbd476e79e8b85bf224daaf4231
Selim <hello@selimbaouz.com>
2025-07-03 20:03:28 +0200

    I replace bg et text color for the sidebar

--- components/navigation/SideBar.tsx
+++ components/navigation/SideBar.tsx
@@ -15,25 +15,25 @@ interface SideBarProps {
 export default function SideBar ({
     menu,
 }: SideBarProps) {
-    const classLink = "font-light text-base text-foreground lg:text-sm xl:text-base group-hover:text-background";
+    const classLink = "font-medium text-base text-white lg:text-sm xl:text-base group-hover:text-background";
     const pathname = usePathname();
     const { isOpenSidebar, setIsOpenSidebar } = useOpenSidebarStore();
 
   return (
     <Sheet open={isOpenSidebar} onOpenChange={setIsOpenSidebar}>
-        <SheetContent side="left" className={cn("h-full min-w-full", "lg:min-w-[500px]")}>
+        <SheetContent side="left" className={cn("h-full min-w-full", "lg:min-w-[500px] bg-primary")}>
         <SheetHeader className="px-4 py-6">
             <SheetTitle className="flex items-center justify-between">
-                <h3 className="text-2xl">
+                <h3 className="text-2xl text-white">
                     Menu
                 </h3>
-                <CgClose className="text-xl" onClick={() => setIsOpenSidebar(false)}/>
+                <CgClose className="text-xl text-white" onClick={() => setIsOpenSidebar(false)}/>
             </SheetTitle>
         </SheetHeader>
         <SheetDescription></SheetDescription>
         <ul className={cn("cursor-pointer")}>
             {menu?.map((data, i) => (
-                <li key={i} className={cn("border-b border-secondary py-3 pl-4 hover:bg-primary group", i === 0 && "border-t", data.path === pathname && "bg-primary")}>
+                <li key={i} className={cn("border-b border-secondary py-3 pl-4 hover:bg-primary group", i === 0 && "border-t", data.path === pathname && "bg-white")}>
                     {data.path.includes("contact") ? (
                         <Link 
                             href="mailto:tailwindliquid@gmail.com"
@@ -46,7 +46,7 @@ export default function SideBar ({
                         href={data.path} 
                         target={data.title === "Suivre ma commande" ? "_blank" : undefined} 
                         rel={data.title === "Suivre ma commande" ? "noopener noreferrer" : undefined} 
-                        className={cn(classLink, data.path === pathname && "text-background dark:text-foreground")}
+                        className={cn(classLink, data.path === pathname && "text-primary font-bold")}
                         onClick={() => {
                             if (!data.path.startsWith("/#")) {
                                 setIsOpenSidebar(false);
